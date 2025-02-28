e180859fb770e0b2f5871e28d99ad6fd97463aab
VoinaRares <voinarares14@gmail.com>
2025-02-28 16:11:21 +0200

    Add chart.js and ng2-charts for expense visualization; implement pie chart in day summary detail component and update styles

--- src/app/day-summary/day-summary-detail/day-summary-detail.component.ts
+++ src/app/day-summary/day-summary-detail/day-summary-detail.component.ts
@@ -2,18 +2,30 @@ import { Component, Input } from '@angular/core';
 import { ExpenseService } from '../../shared/expense.service';
 import { Expense } from '../../shared/expense.interface';
 import { CommonModule } from '@angular/common';
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartOptions } from 'chart.js';
 
 
 @Component({
   selector: 'app-day-summary-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BaseChartDirective],
   templateUrl: './day-summary-detail.component.html',
-  styleUrl: './day-summary-detail.component.css'
+  styleUrl: './day-summary-detail.component.scss'
 })
 export class DaySummaryDetailComponent {
   dailyExpenses: Expense[] = [];
   @Input({ required: true }) day!: string;
+
+  public pieChartOptions: ChartOptions<'pie'> = {
+    responsive: false,
+  };
+  public pieChartLabels: string[] = [];
+  public pieChartDatasets = [{
+    data: [] as number[],
+  }];
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
 
   constructor(public expenseService: ExpenseService) {
     this.expenseService.expenses$.subscribe({
@@ -24,6 +36,22 @@ export class DaySummaryDetailComponent {
 
   ngOnInit() {
     this.dailyExpenses = this.expenseService.getExpensesForDay(this.day);
-    console.log(this.dailyExpenses);
+    this.updateChart();
+  }
+
+  updateChart(){
+    this.pieChartLabels = [...new Set(this.dailyExpenses.map(expense => expense.category))];
+    this.dailyExpenses.forEach(expense => {
+      const index = this.pieChartLabels.indexOf(expense.category);
+      if( this.pieChartDatasets[0].data[index]){
+        this.pieChartDatasets[0].data[index] += expense.amount;
+      }
+      else{
+        this.pieChartDatasets[0].data[index] = expense.amount
+      }
+    }
+    );
+
+    console.log(this.pieChartDatasets[0].data);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { provideRouter } from '@angular/router';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 bootstrapApplication(AppComponent,
   {
-    providers: [provideRouter(routes)]
+    providers: [provideRouter(routes), provideCharts(withDefaultRegisterables())]
   }
 ).catch((err) => console.error(err));
